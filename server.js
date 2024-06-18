cf02dee1446357d80e4851221d4a959a2f2edadb
yusuf <horiyorrmi72@gmail.com>
2024-06-18 19:36:31 +0100

    testing webhook with live

--- server.js
+++ server.js
@@ -138,7 +138,8 @@ app.post("/make-call", (req, res) => {
     task: prompt,
     voice: "2f9fdbc7-4bf2-4792-8a18-21ce3c93978f",
     reduce_latency: false,
-    record:true,
+    webhook: "https://d9a1-102-88-62-91.ngrok-free.app",
+    record: true,
     tools: tools,
     // analysis_schema: {
     //   key_informations: {
