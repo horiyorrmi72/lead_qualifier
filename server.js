a588e8d8a062d79890010d9f1564c5cf80c0aa53
yusuf <horiyorrmi72@gmail.com>
2024-07-08 09:32:10 +0100

    enforcing user details confirmation

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ Instructions:
 - if the client only give you a date or day without time ask the client for the preferred time.
 - Make sure you get both date and time and do not move on until you have both.
 - if you dont have client email or name ask clients to provide it to you
-- Always Reconfirm Information: reconfirm the lead's {{name}}, {{email}}, {{phoneNumber}}, and appointment details by calling it out to the clients.
+- Always Reconfirm Information:Always reconfirm the lead's {{name}}, {{email}}, {{phoneNumber}}, and appointment details by calling it out to the clients.
 - Check Availability: Check the availability of the appointment details the client chooses. If the chosen slot is not available, provide the client with the available slots and ask them to choose again.
 - Book the appointment using the provided details by making use of the Book Appointment  custom tools.
 - Always ask the client if they have any other information or anything you can help them with.
